38b6c70b388dddff18f863afe50f14e04b3616f9
Moises Pelaez <moises.jpelaez@gmail.com>
2023-01-05 16:48:25 -0400

    place Player inside the level for camera setup

--- World/GameEvents.gd
+++ World/GameEvents.gd
@@ -1,20 +1,21 @@
 extends Node
 
 @export var gameplay_events: GameplayEvents
-@export var player_path: NodePath
 
-@onready var player: Player = get_node(player_path)
+@onready var player: Player
 @onready var level_holder : Node = $LevelHolder
 @onready var transition: Transition = $UI/Transition
 
-@export var level: PackedScene
+@export var level_scene: PackedScene
 
 
 func _ready() -> void:
-	level_holder.add_child(level.instantiate())
+	var level = level_scene.instantiate()
+	level_holder.add_child(level)
 	var spawn_pos = get_tree().get_nodes_in_group("spawn")[0].position
+	player = level.get_node("Player")
 	player.position = spawn_pos
-
+	
 	gameplay_events.connect("player_died", _on_GameplayEvents_player_died)
 
 
