494b3845f09eecc3828e2da2b2c7de19390a0c7c
Tibo <goiffonthibaud@protonmail.com>
2023-01-06 14:18:08 +0100

    add: dash zone effect

--- World/DashZone/DashZone.gd
+++ World/DashZone/DashZone.gd
@@ -1,17 +1,38 @@
+@tool
 extends StaticBody2D
 
 @export var gameplay_events: GameplayEvents
+@export var size := Vector2(100,100) : set = _set_size
+
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var area_collision_shape: CollisionShape2D = $Area2D/CollisionShape2D
+@onready var texture : ColorRect = $Texture
 
 
 func _ready() -> void:
 	gameplay_events.connect("dash_started", _on_GameplayEvents_dash_started)
 	gameplay_events.connect("dash_ended", _on_GameplayEvents_dash_end)
-
-
+	get_viewport().connect("size_changed", _set_ratio)
+	texture.connect("resized", _set_ratio)
+	_set_size(size)
+	_set_ratio()
+	
 func _on_GameplayEvents_dash_started() -> void:
 	collision_shape.set_deferred("disabled", true)
-
-
+	
 func _on_GameplayEvents_dash_end() -> void:
 	collision_shape.set_deferred("disabled", false)
+
+func _set_size(value):
+	size = value
+	if !is_inside_tree(): return
+	collision_shape.shape.size = size
+	area_collision_shape.shape.size = size
+	texture.size = size
+	texture.position = -(size/2)
+	_set_ratio()
+
+func _set_ratio():
+	var screen_size = get_viewport().size
+	texture.material.set_shader_parameter("screen_ratio", screen_size.x / screen_size.y)
+	texture.material.set_shader_parameter("ratio", size.x / size.y)
