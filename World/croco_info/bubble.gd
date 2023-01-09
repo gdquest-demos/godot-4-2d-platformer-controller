48e1badab2311b3ff4b437685edf4d63782508bc
Tibo <goiffonthibaud@protonmail.com>
2023-01-09 11:18:48 +0100

    add: croco info

---
+++ World/croco_info/bubble.gd
@@ -0,0 +1,47 @@
+extends MarginContainer
+
+var _current_target  : Node2D = null
+@onready var label = $MarginContainer/Label
+
+var active = false
+var _half_size = Vector2.ONE
+
+func _ready():
+	hide()
+	set_process(false)
+	_set_font_size()
+
+func open(target : Node2D):
+	active = true
+	_current_target = target
+	set_process(true)
+	show()
+	
+func write(line : String):
+	var t = create_tween()
+	t.tween_property(self, "scale", Vector2.ONE, 0.1).from(Vector2.ONE * 0.9).set_trans(Tween.TRANS_BACK)
+	label.text = line
+	# Need to reset size so main container shrinks back?
+	size = Vector2.ZERO
+	
+func close():
+	active = false
+	set_process(false)
+	var t = create_tween()
+	t.tween_property(self, "scale", Vector2.ONE * 0.6, 0.1).from(Vector2.ONE).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
+	await t.finished
+	if !active: hide()
+
+func _process(_delta):
+	position = _current_target.get_global_transform_with_canvas().origin - (_half_size)
+
+func _on_resized():
+	_set_font_size()
+	_half_size = size / 2
+	pivot_offset = _half_size
+	
+func _set_font_size():
+	if !label: return
+	var viewport_size = get_viewport_rect().size
+	var min_size = min(viewport_size.x, viewport_size.y)
+	label.set("theme_override_font_sizes/font_size", min_size * 0.035)
---
+++ World/croco_info/croco_info.gd
@@ -0,0 +1,53 @@
+extends Node2D
+
+@export var lines : PackedStringArray = []
+@onready var sprite = $Sprite2D
+@onready var bubble = $CanvasLayer/Bubble
+@onready var bubble_target = $BubbleTarget
+
+var _is_active = false : set = _set_active
+var _current_line = -1
+var player = null
+
+func _ready():
+	set_process_unhandled_input(_is_active)
+
+func _set_active(value):
+	if _is_active == value: return
+	_is_active = value
+	set_process_unhandled_input(_is_active)
+	if _is_active == false && bubble.active: 
+		_reset_bubble()
+		
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("move_down"):
+		var t = create_tween()
+		t.tween_property(sprite, "scale", Vector2(1.2,0.8), 0.1).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+		t.tween_property(sprite, "scale", Vector2(0.8,1.2), 0.1).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+		t.tween_property(sprite, "scale", Vector2.ONE, 0.15)
+		
+		if _current_line == -1:
+			bubble.open(bubble_target)
+		if _current_line >= lines.size() - 1:
+			_reset_bubble()
+			return
+		_current_line += 1
+		bubble.write(lines[_current_line])
+		_look_at_target(player.global_position)
+
+func _look_at_target(pos : Vector2):
+	sprite.flip_h = (pos.x - self.global_position.x) < 0
+
+func _reset_bubble():
+	_current_line = -1
+	bubble.close()
+
+func _on_area_2d_body_entered(body):
+	if body is Player:
+		player = body
+		_set_active(true)
+
+func _on_area_2d_body_exited(body):
+	if body is Player:
+		player = null
+		_set_active(false)
