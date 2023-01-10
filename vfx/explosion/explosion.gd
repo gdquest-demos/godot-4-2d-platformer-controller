ab3653c90e96d682dcad06ab3662a420284bfc4f
Tibo <goiffonthibaud@protonmail.com>
2023-01-10 21:14:24 +0100

    add: sparkes on explosion

--- vfx/explosion/explosion.gd
+++ vfx/explosion/explosion.gd
@@ -3,11 +3,14 @@ extends Node2D
 signal finished
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
-
+@onready var sparkes : GPUParticles2D = $Sparkes
 
 func _ready():
 	var animations: Array = sprite.frames.get_animation_names()
 	sprite.play(animations.pick_random())
+	sparkes.emitting = true
 	await sprite.animation_finished
+	sprite.queue_free()
 	emit_signal("finished")
+	await get_tree().create_timer(5.0).timeout
 	queue_free()
