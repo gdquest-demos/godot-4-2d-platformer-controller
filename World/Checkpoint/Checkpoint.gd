340287c4d056c7e405d6764216c8f7f4f8698a77
Tibo <goiffonthibaud@protonmail.com>
2023-01-10 17:38:54 +0100

    fix: prevent checkpoint animation if already active

--- World/Checkpoint/Checkpoint.gd
+++ World/Checkpoint/Checkpoint.gd
@@ -11,6 +11,8 @@ func _ready() -> void:
 
 
 func _set_active(value : bool):
+	if active == value: return
+	active = value
 	var t = create_tween()
 	t.tween_property(sprite, "scale", Vector2(1.2,0.8), 0.1).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 	t.tween_property(sprite, "scale", Vector2(0.8,1.2), 0.1).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
